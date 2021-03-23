8e87cf9fc47e2902d00e74ef7ab3548c1681560f
Arturs Slisans <aslisans22@gmail.com>
2021-03-23 14:58:49 +0200

    01

--- 01-show-one-element/QUESTIONS.md
+++ 01-show-one-element/QUESTIONS.md
@@ -6,15 +6,15 @@
 
 > If you click the link to reveal more text and then refresh the page, does the text remain revealed, or is it hidden again? Why?
 
-Your reply here...
+Your reply here... Vins vel projām būs atvērts.
 
 ---
 
 > Remove `window.addEventListener("load", function(){` (and the closing `})`) from **global.js**. Does the link still reveal the text? What is the purpose of this code that you've removed?
 
-Your reply here...
+Your reply here... links vairs nelasīs tavu scriptu bez viņa, tādēļ nedzrīkst viņu dzēst.
 
----
+--- 
 
 > Describe the the `addEventListener` method. (Remember that there is a specific, technical, methodical way to describe methods. Your reply should match that format.)
 
--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -13,5 +13,35 @@ window.addEventListener("load", function(){
   //    from a hidden value to a shown value.
 
   // 5. Also modify the DOM to hide the "More info..." link.
-  
+  <div class="text-container">
+    <h1>DOM automic 01</h1>
+    <h2>DOM automic 01</h2>
+    <div class="content hideContent">
+    The modern mammal metropolis of Zootopia is a city like no other. Comprised of habitat neighborhoods like ritzy Sahara Square and frigid Tundratown, it's a melting pot where animals from every environment live together-a place where no matter what you are, from the biggest elephant to the smallest shrew, you can be anything. But when rookie Officer Judy Hopps (voice of Ginnifer Goodwin) arrives, she discovers that being the first bunny on a police force of big, tough animals isn't so easy. Determined to prove herself, she jumps at the opportunity to crack a case, even if it means partnering with a fast-talking, scam-artist fox, Nick Wilde (voice of Jason Bateman), to solve the mystery.
+        <p>Some more text</p>
+        <ul>
+            <li> Walt Disney Animation Studios' "Zootopia," a comedy-adventure directed by Byron Howard ("Tangled," "Bolt") and Rich Moore ("Wreck-It Ralph," "The Simpsons") and co-directed by Jared Bush ("Penn Zero: Part-Time Hero"), opens in theaters on March 4, 2016. Officer Judy Hopps (voice of Ginnifer Goodwin), the very first bunny on Zootopia's police force, jumps at the opportunity to crack her first case-even if it means partnering with fast-talking, scam-artist fox Nick Wilde (voice of Jason Bateman) to solve the mystery.</li>
+        </ul>
+    </div>
+    <div class="show-more">
+        <a href="#">Show more</a>
+    </div>
+</div>
+ 
+ $(".show-more a").on("click", function() {
+    var $this = $(this); 
+    var $content = $this.parent().prev("div.content");
+    var linkText = $this.text().toUpperCase();    
+
+    if(linkText === "SHOW MORE"){
+        linkText = "Show less";
+        $content.switchClass("hideContent", "showContent", 400);
+    } else {
+        linkText = "Show more";
+        $content.switchClass("showContent", "hideContent", 400);
+    };
+
+    $this.text(linkText);
+});​
+
 });
